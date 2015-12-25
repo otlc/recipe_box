5e82734486c2f996caaed6445d31a2359f79e591
OTLC <onthelinecoders@gmail.com>
2015-12-25 16:30:48 -0500

    Added recipe images, bootstrap styling and scss

--- recipe_box/config/routes.rb
+++ recipe_box/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # Create a resource route for our recipes contoller (maps HTTP verbs to controller actions automatically):
+  resources :recipes
+
+  # Map the root of our recipe site to 'recipes#index'
+  root 'recipes#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
