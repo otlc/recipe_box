1069748cbbf0e9326c56f8ac874fd5f589e37dfb
OTLC <onthelinecoders@gmail.com>
2015-12-25 23:45:15 -0500

    Fix recipe.rb model to include :image on all forms

--- recipe_box/app/models/recipe.rb
+++ recipe_box/app/models/recipe.rb
@@ -10,9 +10,9 @@ class Recipe < ActiveRecord::Base
   															reject_if: proc { |attributes| attributes['step'].blank? },
   															allow_destroy: true
 
-  validates :title, :description, presence: true
+  validates :title, :description, :image, presence: true
 
 	has_attached_file :image, styles: { :medium => "400x400#" }
-	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/	
+  
 end
